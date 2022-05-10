22a7aa85d4b619247a07a34996af42a01b001ed7
Benjamin <benjaminking1337@live.com>
2022-05-10 13:05:16 +0200

    deploy test 3

--- Backend/test/workflow.test.js
+++ Backend/test/workflow.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 const chaiHttp = require("chai-http");
-const server = require("./Backend/server");
+const server = require("../server");
 
 chai.use(chaiHttp);
 
